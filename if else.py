8cc4b1d2ccec38a392d23be4a16d7ea5c775e702
Takanashi-Daffa <r.daffaarkhabista@gmail.com>
2025-09-10 16:20:47 +0800

    Add files via upload

---
+++ if else.py
@@ -0,0 +1,7 @@
+x = int(input("masukan nilai x: "))
+if x <= 10:
+    print("nilai x kurang dari sama dengan 10")
+elif x >=40:
+    print("nilai x lebih dari sama dengan 40")
+else:
+    print("nilai lebih dari 10 dan kurang dari 40")
